abbff52b69e0e3b1241d34a281faca0ad3e4b5e9
Montagne Allan <allan.montagne@etu.umontpellier.fr>
2023-12-08 00:16:15 +0100

    add Back

---
+++ Back/src/main/java/oc/projet3/demo/P3JavaApplication.java
@@ -0,0 +1,13 @@
+package oc.projet3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3JavaApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/oc/projet3/demo/P3JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package oc.projet3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class P3JavaApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
