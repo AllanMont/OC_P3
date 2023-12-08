ace34e0751d8cd448e03635c0125b6116c546d75
AllanMont <allan.mont34@gmail.com>
2023-12-08 02:24:58 +0100

    initialise Spring Boot Security

--- Back/src/main/java/oc/projet3/demo/P3JavaApplication.java
+++ Back/src/main/java/oc/projet3/demo/P3JavaApplication.java
@@ -1,13 +1,20 @@
 package oc.projet3.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P3JavaApplication {
+public class P3JavaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(P3JavaApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application Started");
 	}
 
 }
