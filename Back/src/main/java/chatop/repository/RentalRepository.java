5521fde5a76bab3c9bc0df0520979f656bcd3dac
AllanMont <allan.mont34@gmail.com>
2023-12-13 23:40:52 +0100

    complete rental service

--- Back/src/main/java/chatop/repository/RentalRepository.java
+++ Back/src/main/java/chatop/repository/RentalRepository.java
@@ -3,7 +3,9 @@ package chatop.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import chatop.model.rental;
+
 @Repository
-public interface RentalRepository extends CrudRepository<Rental, Long> {
+public interface RentalRepository extends CrudRepository<rental, Long> {
 
 }
--- Back/src/main/java/chatop/service/RentalService.java
+++ Back/src/main/java/chatop/service/RentalService.java
@@ -1,8 +1,11 @@
 package chatop.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import chatop.model.rental;
 import chatop.repository.RentalRepository;
 import lombok.Data;
 
@@ -13,9 +16,17 @@ public class RentalService {
   @Autowired
   private RentalRepository rentalRepository;
   
-    public void getAllRentals() {
-        
-    }
+  public Iterable<rental> getAllRentals() {
+      return rentalRepository.findAll();
+  }
 
+  public Optional<rental> getOneRentalById(final Long id) {
+      return rentalRepository.findById(id);
+  }
+
+  public rental newRental(rental Rental) {
+      rental savedRental = rentalRepository.save(Rental);
+      return savedRental;
+  }
 
 }
