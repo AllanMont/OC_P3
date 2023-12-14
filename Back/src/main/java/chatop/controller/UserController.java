09b0f48f0574514c6da362dd002e3609987aa4c9
AllanMont <allan.mont34@gmail.com>
2023-12-14 01:35:25 +0100

    Feat(User)New Routes to getInfosUserById

--- Back/src/main/java/chatop/controller/UserController.java
+++ Back/src/main/java/chatop/controller/UserController.java
@@ -1,15 +1,37 @@
 package chatop.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import chatop.model.user;
+import chatop.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 	
+	private final UserService userService;
+	
+  	@Autowired
+  	public UserController(UserService userService) {
+      this.userService = userService;
+  	}
+  	
 	@GetMapping("/{id}")
-	public void getInfosUser() {
-		
+	public ResponseEntity<user> getInfosUserById(@PathVariable Long id) {
+	    Optional<user> optionalUser = userService.getInfosUserById(id);
+
+	    if (optionalUser.isPresent()) {
+	        user foundUser = optionalUser.get();
+	        return ResponseEntity.ok(foundUser);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
 	}
 }
--- Back/src/main/java/chatop/model/user.java
+++ Back/src/main/java/chatop/model/user.java
@@ -11,7 +11,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class user {
 
 
